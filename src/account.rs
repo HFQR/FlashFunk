d0b5a857ba46d46556d3228a18f25fe07853eab8
somewheve <somewheve@gmail.com>
2020-08-13 23:55:08 +0800

    remove fuck warning

--- src/account.rs
+++ src/account.rs
@@ -1,6 +1,7 @@
+#![allow(dead_code, unused_variables)]
+
 use std::collections::hash_map::RandomState;
 use std::collections::HashMap;
-use std::ops::{DerefMut, Deref};
 
 use crate::structs::{AccountData, OrderData, Params, TickData, TradeData, DailyResult};
 use std::borrow::Borrow;
@@ -22,7 +23,8 @@ impl PositionManager {
 
 /// Account Instance
 /// It provides most public API to Accept data or solve data
-/// 
+
+///
 pub struct Account {
     name: f64,
     pre_balance: f64,
@@ -46,24 +48,29 @@ pub struct Account {
 
 
 impl Account {
-    fn available(&mut self) -> f64 {
-        let frozen_fee: f64 = self.frozen_fee.values().into_iter().sum();
-        let fee: f64 = self.get_fee_sum();
-        let close_profit: f64 = self.close_profit.values().into_iter().map(|x| { x.clone() }).collect::<Vec<f64>>().iter().sum();
-        self.pre_balance + self.float_pnl() + close_profit - frozen_fee - fee - self.margin() - self.frozen_margin()
+    pub fn balance(&mut self) -> f64 {
+        self.available() + self.margin()
     }
-    fn get_fee_sum(&mut self) -> f64 {
+    pub fn available(&mut self) -> f64 {
+        self.pre_balance + self.float_pnl() + self.get_close_profit_sum() - self.get_frozen_fee_sum() - self.get_fee_sum() - self.margin() - self.frozen_margin()
+    }
+    pub fn get_fee_sum(&mut self) -> f64 {
         self.fee.values().into_iter().map(|x| { x.clone() }).collect::<Vec<f64>>().iter().sum()
     }
-    fn balance(&mut self) -> f64 {
-        self.available() + self.margin()
+
+    pub fn get_frozen_fee_sum(&mut self) -> f64 {
+        self.frozen_fee.values().into_iter().sum()
+    }
+
+    pub fn get_close_profit_sum(&mut self) -> f64 {
+        self.close_profit.values().into_iter().map(|x| { x.clone() }).collect::<Vec<f64>>().iter().sum()
     }
     /// update trade
     /// 1. add fee to actual fee
     /// 2.remove frozen_fee if exist
     /// 3.remove frozen if exist
     /// 4. add close_profit
-    fn update_trade(&mut self, data: TradeData) {
+    pub fn update_trade(&mut self, data: TradeData) {
         let symbol: String = data.symbol.clone().unwrap().to_string();
         // calculate fee for trade_data
         let commision = data.volume * data.price * *self.get_commission_ratio(symbol.as_str());
@@ -76,7 +83,8 @@ impl Account {
         }
         // insert fee to fact
         if let Some(t) = self.fee.get(symbol.as_str()) {
-            self.fee.insert(symbol.clone(), commision + *t);
+            let sum = commision + *t;
+            self.fee.insert(symbol.clone(), sum);
         } else {
             self.fee.insert(symbol.clone(), commision);
         }
@@ -103,7 +111,8 @@ impl Account {
                     Direction::NET => { 0.0 }
                 };
                 if let Some(t) = self.close_profit.get(&symbol) {
-                    self.close_profit.insert(symbol.clone(), close_profit + *t);
+                    let sum = close_profit + *t;
+                    self.close_profit.insert(symbol.clone(), sum);
                 } else {
                     self.close_profit.insert(symbol.clone(), close_profit);
                 }
@@ -125,7 +134,7 @@ impl Account {
     /// update order
     /// 1.add frozen fee if open
     /// 2.add margin_frozen if open
-    fn update_order(&mut self, data: OrderData) {
+    pub fn update_order(&mut self, data: OrderData) {
         let symbol: String = data.symbol.clone().unwrap().to_string();
         let commission_ratio = self.get_commission_ratio(&symbol).clone();
         self.frozen_fee.insert(symbol.clone(), commission_ratio * data.volume * data.price);
@@ -140,11 +149,11 @@ impl Account {
     }
     /// update position by tick
     /// refresh pnl in time
-    fn update_tick(&mut self, tick: TickData) { unimplemented!() }
+    pub fn update_tick(&mut self, tick: TickData) { unimplemented!() }
     ///  get the float pnl for account
-    fn float_pnl(&mut self) -> f64 { unimplemented!() }
+    pub fn float_pnl(&mut self) -> f64 { unimplemented!() }
     ///  get the margin of position for the account
-    fn margin(&mut self) -> f64 {
+    pub fn margin(&mut self) -> f64 {
         let mut rs = 0.0;
 
         for pos in self.position_manager.get_all_positions() {
@@ -156,12 +165,12 @@ impl Account {
         rs
     }
     /// settle the account by passed a datetime
-    fn settle(&mut self) -> bool { unimplemented!() }
+    pub fn settle(&mut self) -> bool { unimplemented!() }
     /// update the params by pass a Params
     /// it looks like hard to understand
     fn update_params(&mut self, params: Params) { unimplemented!() }
     /// get the frozen , when day,end ,it will zero
-    fn frozen_margin(&mut self) -> f64 { unimplemented!() }
+    pub fn frozen_margin(&mut self) -> f64 { unimplemented!() }
 
     /// generator a Account object named DailyResult, it will be written into database
     fn generate_self(&mut self) -> DailyResult {
@@ -169,7 +178,7 @@ impl Account {
             available: self.available(),
             balance: self.balance(),
             fee: self.get_fee_sum(),
-            margin: 0.0,
+            margin: self.margin(),
             date: "".to_string(),
         }
     }
