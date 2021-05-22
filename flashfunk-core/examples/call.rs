3ddef966d507a46e8de0e9590d3191e9a0b9a32b
somewheve <somewheve@gmail.com>
2021-05-23 03:50:15 +0800

    update example

--- flashfunk-core/examples/call.rs
+++ flashfunk-core/examples/call.rs
@@ -8,108 +8,22 @@ use flashfunk_level::CtpMdApi;
 use flashfunk_level::CtpTdApi;
 use flashfunk_core::prelude::*;
 
-/// 價格
-/// Now we build a order book and calculate the  trend power
-struct Quote {
-    ask: f64,
-    bid: f64,
-    bid_volume: f64,
-    ask_volume: f64,
-    thread: f64,
-    last_price: f64,
-    value: f64,
-}
-
-impl Quote {
-    pub fn new() -> Quote {
-        Quote {
-            ask: 0.0,
-            bid: 0.0,
-            bid_volume: 0.0,
-            ask_volume: 0.0,
-            thread: 0.0,
-            last_price: 0.0,
-            value: 0.0,
-        }
-    }
-    /// 計算盤口信息
-    pub fn update_tick(&mut self, tick: &TickData) -> f64 {
-        let ask_1 = tick.ask_price(0);
-        let bid_1 = tick.bid_price(0);
-        let ask_vol_1 = tick.ask_volume(0);
-        let bid_vol_1 = tick.bid_volume(0);
-        // when price is down just, increase\+
-        // let value  = if ask_1 > self.ask {
-        // price u
-
-
-        self.value = self.value;
-        self.value
-    }
-}
-
 struct Strategy {
-    quote: Quote,
+    local_symbol: Vec<String>,
 }
 
 impl Ac for Strategy {
     fn on_tick(&mut self, tick: &TickData, ctx: &mut Context) {
-        let pos = ctx.get_position(&tick.symbol);
-        let is_send_long = true;
-        let is_send_short = true;
-        let req = OrderRequest {
-            symbol: "OI101".to_string(),
-            exchange: Exchange::SHFE,
-            direction: Direction::LONG,
-            order_type: OrderType::LIMIT,
-            volume: 1.0,
-            price: tick.last_price - 1.0,
-            offset: Offset::OPEN,
-            reference: None,
-        };
-        ctx.send(req);
-
-        ctx.enter(|x, v| {
-            let pos = v.get_position(&"OI101".to_string());
-            if pos.short_volume != 0.0 && is_send_long == false {
-                let req = OrderRequest {
-                    symbol: "OI101".to_string(),
-                    exchange: Exchange::CZCE,
-                    direction: Direction::LONG,
-                    order_type: OrderType::LIMIT,
-                    volume: pos.short_volume,
-                    price: tick.last_price + 5.0,
-                    offset: Offset::CLOSETODAY,
-                    reference: None,
-                };
-                x.send(req);
-            }
-            if pos.long_volume != 0.0 && is_send_short == false {
-                let req = OrderRequest {
-                    symbol: "OI101".to_string(),
-                    exchange: Exchange::CZCE,
-                    direction: Direction::SHORT,
-                    order_type: OrderType::LIMIT,
-                    volume: pos.long_volume,
-                    price: tick.last_price - 3.0,
-                    offset: Offset::CLOSETODAY,
-                    reference: None,
-                };
-                x.send(req);
-            }
-        });
-        self.quote.update_tick(tick);
         println!("dur: {}", tick.instant.elapsed().as_nanos());
     }
 
-    fn on_order(&mut self, order: &OrderData, ctx: &mut Context) {
-        println!("Order 回報: {:?}", order);
-    }
     fn local_symbols<'a>(&mut self) -> Vec<&'a str> {
-        vec!["rb2110"]
+        let mut strs: Vec<&'static str> = Vec::new();
+        self.local_symbol.iter().for_each(|x| strs.push(Box::leak(x.clone().into_boxed_str())));
+        strs
     }
 
-    fn name<'a>(&mut self) -> &'a str { "hello" }
+    fn name<'a>(&mut self) -> &'a str { "oi" }
 }
 
 fn main() {
@@ -118,17 +32,16 @@ fn main() {
         .password("wi1015..")
         .broke_id("9999")
         .app_id("simnow_client_test")
-        .md_address("tcp://180.168.146.187:10131")
-        .td_address("tcp://180.168.146.187:10130")
+        .md_address("tcp://218.202.237.33:10213")
+        .td_address("tcp://218.202.237.33:10203")
         .auth_code("0000000000000000")
         .production_info("");
     let strategy_1 = Strategy {
-        quote: Quote::new(),
+        local_symbol: vec!["rb2110".to_string()]
     };
     Flash::builder::<CtpMdApi, CtpTdApi, _>("ctpbee")
         .strategies(vec![Box::new(strategy_1)])
-        .id("name")
-        .pwd("id")
+        .id("flashfunk")
         .login_form(login_form)
         .start();
 }
