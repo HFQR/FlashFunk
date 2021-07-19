1f06860cbc8d315dc1633b603621995e7effa78f
fakeshadow <24548779@qq.com>
2021-07-19 12:02:17 +0800

    increase test coverage for stackgroup

--- flashfunk-core/src/util/channel.rs
+++ flashfunk-core/src/util/channel.rs
@@ -249,6 +249,7 @@ mod test {
         assert_eq!(iter.next(), Some(&1));
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), None);
     }
 
     #[test]
