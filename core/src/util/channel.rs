8ce332bd277130d6527acc2ece50d7ac69e1a8a6
fakeshadow <24548779@qq.com>
2021-10-02 15:45:35 +0800

    reduce duplicate code between features

--- core/src/util/channel.rs
+++ core/src/util/channel.rs
@@ -57,6 +57,29 @@ pub struct Sender<M> {
     waker: Arc<AtomicWaker>,
 }
 
+impl<M> Sender<M> {
+    // 发送失败会panic
+    #[inline]
+    pub fn send(&self, m: impl Into<M>) {
+        self.tx.push(m.into()).unwrap();
+        #[cfg(feature = "async")]
+        self.waker.wake();
+    }
+
+    // 发送失败返回消息
+    #[inline]
+    pub fn try_send(&self, m: M) -> Result<(), ChannelError<M>> {
+        match self.tx.push(m) {
+            Ok(_) => {
+                #[cfg(feature = "async")]
+                self.waker.wake();
+                Ok(())
+            }
+            Err(e) => Err(ChannelError::TrySendError(e.0)),
+        }
+    }
+}
+
 pub struct Receiver<M> {
     rx: Consumer<M>,
     #[cfg(feature = "async")]
@@ -75,113 +98,10 @@ mod r#sync {
         (Sender { tx }, Receiver { rx })
     }
 
-    impl<M> Sender<M> {
-        // 发送失败会panic
-        #[inline]
-        pub fn send(&self, m: impl Into<M>) {
-            self.tx.push(m.into()).unwrap();
-        }
-
-        // 发送失败返回消息
-        #[inline]
-        pub fn try_send(&self, m: M) -> Result<(), ChannelError<M>> {
-            self.tx.push(m).map_err(|e| ChannelError::TrySendError(e.0))
-        }
-    }
-
     impl<M> Receiver<M> {
         #[inline]
         pub fn recv(&self) -> Result<M, ChannelError<M>> {
             self.rx.pop().map_err(|_| ChannelError::RecvError)
-        }
-    }
-
-    pub struct GroupSender<M, const N: usize> {
-        senders: StackArray<Sender<M>, N>,
-        group: AHashMap<&'static str, Vec<usize>>,
-    }
-
-    impl<M, const N: usize> GroupSender<M, N> {
-        pub fn new(sender: Vec<Sender<M>>, group: AHashMap<&'static str, Vec<usize>>) -> Self {
-            Self {
-                senders: StackArray::from_vec(sender),
-                group,
-            }
-        }
-
-        #[inline]
-        pub fn group(&self) -> &AHashMap<&'static str, Vec<usize>> {
-            &self.group
-        }
-
-        #[inline]
-        pub fn senders(&self) -> &[Sender<M>] {
-            &*self.senders
-        }
-
-        // 发送至所有sender
-        #[inline]
-        pub fn send_all<MM>(&self, mm: MM)
-        where
-            MM: Into<M> + Clone,
-        {
-            self.senders.iter().for_each(|s| s.send(mm.clone().into()))
-        }
-
-        // 发送至指定index的sender. 失败会panic
-        #[inline]
-        pub fn send_to(&self, m: impl Into<M>, sender_index: usize) {
-            self.senders[sender_index].send(m.into());
-        }
-
-        // 发送至指定index的sender. 失败会返回消息
-        #[inline]
-        pub fn try_send_to<MM>(&self, m: MM, sender_index: usize) -> Result<(), ChannelError<MM>>
-        where
-            MM: Into<M>,
-        {
-            match self.senders.get(sender_index) {
-                Some(s) => {
-                    s.send(m.into());
-                    Ok(())
-                }
-                None => Err(ChannelError::SenderOverFlow(m)),
-            }
-        }
-
-        // 发送至指定group. group查找失败失败会返回消息.(group内的sender发送失败会panic)
-        #[inline]
-        pub fn try_send_group<MM>(&self, mm: MM, symbol: &str) -> Result<(), ChannelError<MM>>
-        where
-            MM: Into<M> + Clone,
-        {
-            match self.group.get(symbol) {
-                Some(g) => {
-                    g.iter().for_each(|i| self.send_to(mm.clone(), *i));
-                    Ok(())
-                }
-                None => Err(ChannelError::SenderGroupNotFound(mm)),
-            }
-        }
-    }
-
-    pub struct GroupReceiver<M, const N: usize> {
-        receivers: StackArray<Receiver<M>, N>,
-    }
-
-    impl<M, const N: usize> GroupReceiver<M, N> {
-        pub(crate) fn from_vec(vec: Vec<Receiver<M>>) -> Self {
-            Self {
-                receivers: StackArray::from_vec(vec),
-            }
-        }
-    }
-
-    impl<M, const N: usize> Deref for GroupReceiver<M, N> {
-        type Target = [Receiver<M>];
-
-        fn deref(&self) -> &Self::Target {
-            self.receivers.deref()
         }
     }
 }
@@ -211,24 +131,6 @@ mod r#async {
         )
     }
 
-    impl<M> Sender<M> {
-        // 发送失败会panic
-        #[inline]
-        pub fn send(&self, m: impl Into<M>) {
-            self.tx.push(m.into()).unwrap();
-            self.waker.wake();
-        }
-
-        // 发送失败返回消息
-        #[inline]
-        pub fn try_send(&self, m: M) -> Result<(), ChannelError<M>> {
-            self.tx
-                .push(m)
-                .map(|_| self.waker.wake())
-                .map_err(|e| ChannelError::TrySendError(e.0))
-        }
-    }
-
     impl<M> Receiver<M> {
         #[inline]
         pub async fn recv(&self) -> Result<M, ChannelError<M>> {
@@ -251,93 +153,93 @@ mod r#async {
             Recv(self).await
         }
     }
+}
 
-    pub struct GroupSender<M, const N: usize> {
-        senders: StackArray<Sender<M>, N>,
-        group: AHashMap<&'static str, Vec<usize>>,
-    }
+pub struct GroupSender<M, const N: usize> {
+    senders: StackArray<Sender<M>, N>,
+    group: AHashMap<&'static str, Vec<usize>>,
+}
 
-    impl<M, const N: usize> GroupSender<M, N> {
-        pub fn new(sender: Vec<Sender<M>>, group: AHashMap<&'static str, Vec<usize>>) -> Self {
-            Self {
-                senders: StackArray::from_vec(sender),
-                group,
-            }
-        }
-
-        #[inline]
-        pub fn group(&self) -> &AHashMap<&'static str, Vec<usize>> {
-            &self.group
-        }
-
-        #[inline]
-        pub fn senders(&self) -> &[Sender<M>] {
-            &*self.senders
-        }
-
-        // 发送至所有sender
-        #[inline]
-        pub fn send_all<MM>(&self, mm: MM)
-        where
-            MM: Into<M> + Clone,
-        {
-            self.senders.iter().for_each(|s| s.send(mm.clone().into()))
-        }
-
-        // 发送至指定index的sender. 失败会panic
-        #[inline]
-        pub fn send_to(&self, m: impl Into<M>, sender_index: usize) {
-            self.senders[sender_index].send(m.into());
-        }
-
-        // 发送至指定index的sender. 失败会返回消息
-        #[inline]
-        pub fn try_send_to<MM>(&self, m: MM, sender_index: usize) -> Result<(), ChannelError<MM>>
-        where
-            MM: Into<M>,
-        {
-            match self.senders.get(sender_index) {
-                Some(s) => {
-                    s.send(m.into());
-                    Ok(())
-                }
-                None => Err(ChannelError::SenderOverFlow(m)),
-            }
-        }
-
-        // 发送至指定group. group查找失败失败会返回消息.(group内的sender发送失败会panic)
-        #[inline]
-        pub fn try_send_group<MM>(&self, mm: MM, symbol: &str) -> Result<(), ChannelError<MM>>
-        where
-            MM: Into<M> + Clone,
-        {
-            match self.group.get(symbol) {
-                Some(g) => {
-                    g.iter().for_each(|i| self.send_to(mm.clone(), *i));
-                    Ok(())
-                }
-                None => Err(ChannelError::SenderGroupNotFound(mm)),
-            }
+impl<M, const N: usize> GroupSender<M, N> {
+    pub fn new(sender: Vec<Sender<M>>, group: AHashMap<&'static str, Vec<usize>>) -> Self {
+        Self {
+            senders: StackArray::from_vec(sender),
+            group,
         }
     }
 
-    pub struct GroupReceiver<M, const N: usize> {
-        receivers: StackArray<Receiver<M>, N>,
+    #[inline]
+    pub fn group(&self) -> &AHashMap<&'static str, Vec<usize>> {
+        &self.group
     }
 
-    impl<M, const N: usize> GroupReceiver<M, N> {
-        pub(crate) fn from_vec(vec: Vec<Receiver<M>>) -> Self {
-            Self {
-                receivers: StackArray::from_vec(vec),
+    #[inline]
+    pub fn senders(&self) -> &[Sender<M>] {
+        &*self.senders
+    }
+
+    // 发送至所有sender
+    #[inline]
+    pub fn send_all<MM>(&self, mm: MM)
+    where
+        MM: Into<M> + Clone,
+    {
+        self.senders.iter().for_each(|s| s.send(mm.clone().into()))
+    }
+
+    // 发送至指定index的sender. 失败会panic
+    #[inline]
+    pub fn send_to(&self, m: impl Into<M>, sender_index: usize) {
+        self.senders[sender_index].send(m.into());
+    }
+
+    // 发送至指定index的sender. 失败会返回消息
+    #[inline]
+    pub fn try_send_to<MM>(&self, m: MM, sender_index: usize) -> Result<(), ChannelError<MM>>
+    where
+        MM: Into<M>,
+    {
+        match self.senders.get(sender_index) {
+            Some(s) => {
+                s.send(m.into());
+                Ok(())
             }
+            None => Err(ChannelError::SenderOverFlow(m)),
         }
     }
 
-    impl<M, const N: usize> Deref for GroupReceiver<M, N> {
-        type Target = [Receiver<M>];
-
-        fn deref(&self) -> &Self::Target {
-            self.receivers.deref()
+    // 发送至指定group. group查找失败失败会返回消息.(group内的sender发送失败会panic)
+    #[inline]
+    pub fn try_send_group<MM>(&self, mm: MM, symbol: &str) -> Result<(), ChannelError<MM>>
+    where
+        MM: Into<M> + Clone,
+    {
+        match self.group.get(symbol) {
+            Some(g) => {
+                g.iter().for_each(|i| self.send_to(mm.clone(), *i));
+                Ok(())
+            }
+            None => Err(ChannelError::SenderGroupNotFound(mm)),
         }
+    }
+}
+
+pub struct GroupReceiver<M, const N: usize> {
+    receivers: StackArray<Receiver<M>, N>,
+}
+
+impl<M, const N: usize> GroupReceiver<M, N> {
+    pub(crate) fn from_vec(vec: Vec<Receiver<M>>) -> Self {
+        Self {
+            receivers: StackArray::from_vec(vec),
+        }
+    }
+}
+
+impl<M, const N: usize> Deref for GroupReceiver<M, N> {
+    type Target = [Receiver<M>];
+
+    fn deref(&self) -> &Self::Target {
+        self.receivers.deref()
     }
 }
