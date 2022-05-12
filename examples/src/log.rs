3bed6715ebbe7646655fd1a0c857d541addf0b4d
fakeshadow <24548779@qq.com>
2022-05-12 12:17:53 +0800

    add thread safe logger.

--- examples/src/log.rs
+++ examples/src/log.rs
@@ -1,24 +1,27 @@
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+use std::any::Any;
 use std::sync::{
     atomic::{AtomicBool, Ordering},
     Arc,
 };
 
 use core_affinity::{set_for_current, CoreId};
-use flashfunk_core::util::channel::{channel, Sender};
+use crossbeam_queue::ArrayQueue;
 use owned_log::{OwnedLog, Value};
 
 fn main() {
     MyLogger::with_handler(
         |mut value| {
+            let value = value.downcast_mut::<MyValue>().unwrap();
+            println!("value is {:?}", value.0);
             value.display();
         },
         Some(1),
     );
 
-    struct MyValue;
+    struct MyValue(usize);
 
     impl Value for MyValue {
         fn display(&mut self) {
@@ -27,7 +30,7 @@ fn main() {
     }
 
     for _ in 0..99 {
-        owned_log::log!(Box::new(MyValue));
+        owned_log::log!(Box::new(MyValue(1)));
     }
 
     let flag = Arc::new(AtomicBool::new(false));
@@ -45,7 +48,7 @@ fn main() {
 
     while time < 8 {
         if flag.swap(false, Ordering::SeqCst) {
-            let value = Box::new(MyValue) as _;
+            let value = Box::new(MyValue(2)) as _;
             let now = std::time::Instant::now();
             owned_log::log!(value);
             total += now.elapsed().as_nanos();
@@ -58,11 +61,11 @@ fn main() {
     handle.join().unwrap();
 }
 
-struct MyLogger(Sender<Box<dyn Value>>);
+struct MyLogger(Arc<ArrayQueue<Box<dyn Value>>>);
 
 impl OwnedLog for MyLogger {
     fn log(&self, value: Box<dyn Value>) {
-        self.0.send(value)
+        self.0.push(value).ok().unwrap();
     }
 }
 
@@ -71,10 +74,10 @@ impl MyLogger {
     where
         F: FnMut(Box<dyn Value>) -> () + Send + 'static,
     {
-        let (tx, rx) = channel(256);
+        let queue = Arc::new(ArrayQueue::new(256));
 
         owned_log::OWNED_LOGGER
-            .with(|logger| logger.set(Box::new(MyLogger(tx)) as _))
+            .set(Arc::new(MyLogger(queue.clone())) as _)
             .ok()
             .unwrap();
 
@@ -82,9 +85,9 @@ impl MyLogger {
             set_for_current(CoreId {
                 id: id.unwrap_or(0),
             });
-            match rx.recv() {
-                Ok(msg) => func(msg),
-                Err(_) => std::thread::sleep(std::time::Duration::from_millis(100)),
+            match queue.pop() {
+                Some(msg) => func(msg),
+                None => std::thread::sleep(std::time::Duration::from_millis(100)),
             }
         });
     }
--- log/src/lib.rs
+++ log/src/lib.rs
@@ -1,31 +1,96 @@
 mod no_op;
 
-pub trait OwnedLog: 'static {
+use std::{any::TypeId, sync::Arc};
+
+use once_cell::sync::OnceCell;
+
+pub trait OwnedLog: Send + Sync + 'static {
     fn log(&self, value: Box<dyn Value>);
 }
 
 pub trait Value: Send + 'static {
     fn display(&mut self);
+
+    #[doc(hidden)]
+    #[allow(dead_code)]
+    fn __private_get_type_id__(&self, _: PrivateHelper) -> (TypeId, PrivateHelper)
+    where
+        Self: 'static,
+    {
+        (TypeId::of::<Self>(), PrivateHelper(()))
+    }
 }
 
-use once_cell::unsync::OnceCell;
+#[doc(hidden)]
+#[allow(dead_code)]
+pub struct PrivateHelper(());
 
-thread_local! {
-    pub static OWNED_LOGGER: OnceCell<Box<dyn OwnedLog>> = OnceCell::new();
+impl dyn Value {
+    #[allow(dead_code)]
+    pub fn downcast_ref<T: Value>(&self) -> Option<&T> {
+        if self.__private_get_type_id__(PrivateHelper(())).0 == TypeId::of::<T>() {
+            // SAFETY: external crates cannot override the default
+            // implementation of `__private_get_type_id__`, since
+            // it requires returning a private type. We can therefore
+            // rely on the returned `TypeId`, which ensures that this
+            // case is correct.
+            unsafe { Some(&*(self as *const dyn Value as *const T)) }
+        } else {
+            None
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn downcast_mut<T: Value>(&mut self) -> Option<&mut T> {
+        if self.__private_get_type_id__(PrivateHelper(())).0 == TypeId::of::<T>() {
+            // SAFETY: external crates cannot override the default
+            // implementation of `__private_get_type_id__`, since
+            // it requires returning a private type. We can therefore
+            // rely on the returned `TypeId`, which ensures that this
+            // case is correct.
+            unsafe { Some(&mut *(self as *mut dyn Value as *mut T)) }
+        } else {
+            None
+        }
+    }
 }
+
+pub static OWNED_LOGGER: OnceCell<Arc<dyn OwnedLog>> = OnceCell::new();
 
 #[macro_export]
 macro_rules! log {
     ($value: expr) => {
-        ::owned_log::OWNED_LOGGER.with(|logger| {
-            logger
-                .get_or_init(|| Box::new(::owned_log::__private::NoOpLogger))
-                .log($value)
-        });
+        ::owned_log::__private::OWNED_LOGGER_LOCAL.with(|logger| logger.log($value));
     };
 }
 
 #[doc(hidden)]
 pub mod __private {
-    pub use crate::no_op::NoOpLogger;
+    use super::*;
+
+    thread_local! {
+        pub static OWNED_LOGGER_LOCAL: Arc<dyn OwnedLog> = {
+            OWNED_LOGGER.get_or_init(|| Arc::new(crate::no_op::NoOpLogger)).clone()
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_any_casting() {
+        struct MyValue;
+
+        impl Value for MyValue {
+            fn display(&mut self) {
+                todo!()
+            }
+        }
+
+        let mut value = Box::new(MyValue) as Box<dyn Value>;
+
+        value.downcast_mut::<MyValue>().unwrap();
+    }
 }
